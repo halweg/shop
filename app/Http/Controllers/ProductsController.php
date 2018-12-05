f6f0bd080b2b0bac44584438443a930fa95194c2
Haowei <8688534haowei@gmail.com>
2018-12-05 17:42:34 +0800

    顶部导航加入商品类目树

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -12,13 +12,9 @@ class ProductsController extends Controller
 {
     public function index(Request $request)
     {
-        // 创建一个查询构造器
         $builder = Product::query()->where('on_sale', true);
-        // 判断是否有提交 search 参数，如果有就赋值给 $search 变量
-        // search 参数用来模糊搜索商品
         if ($search = $request->input('search', '')) {
             $like = '%'.$search.'%';
-            // 模糊搜索商品标题、商品详情、SKU 标题、SKU描述
             $builder->where(function ($query) use ($like) {
                 $query->where('title', 'like', $like)
                     ->orWhere('description', 'like', $like)
@@ -30,27 +26,18 @@ class ProductsController extends Controller
         }
 
         if ($request->input('category_id') && $category = Category::find($request->input('category_id'))) {
-            // 如果这是一个父类目
             if ($category->is_directory) {
-                // 则筛选出该父类目下所有子类目的商品
                 $builder->whereHas('category', function ($query) use ($category) {
-                    // 这里的逻辑参考本章第一节
                     $query->where('path', 'like', $category->path.$category->id.'-%');
                 });
             } else {
-                // 如果这不是一个父类目，则直接筛选此类目下的商品
                 $builder->where('category_id', $category->id);
             }
         }
 
-        // 是否有提交 order 参数，如果有就赋值给 $order 变量
-        // order 参数用来控制商品的排序规则
         if ($order = $request->input('order', '')) {
-            // 是否是以 _asc 或者 _desc 结尾
             if (preg_match('/^(.+)_(asc|desc)$/', $order, $m)) {
-                // 如果字符串的开头是这 3 个字符串之一，说明是一个合法的排序值
                 if (in_array($m[1], ['price', 'sold_count', 'rating'])) {
-                    // 根据传入的排序值来构造排序参数
                     $builder->orderBy($m[1], $m[2]);
                 }
             }
